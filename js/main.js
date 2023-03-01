880e0665a274e24c10562a19b828f5e0428f335d
NTNawshin <nawshin@wisc.edu>
2023-03-01 09:48:45 -0600

    Activity 6

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-//declare map var in global scope
+//declare map variable globally so all functions have access
 var map;
-//function to instantiate the Leaflet map
-function createMap() {
+var minValue;
+
+function createMap(){
+
     //create the map
     map = L.map('map', {
-        center: [20, 0],
+        center: [0, 0],
         zoom: 2
     });
 
@@ -15,7 +17,7 @@ function createMap() {
     }).addTo(map);
 
     //call getData function
-    getData();
+    getData(map);
 };
 
 function onEachFeature(feature, layer) {
@@ -30,37 +32,185 @@ function onEachFeature(feature, layer) {
     };
 };
 
-//function to retrieve the data and place it on the map
-function getData() {
+function calculateMinValue(data){
+    //create empty array to store all data values
+    var allValues = [];
+    //loop through each city
+    for(var country of data.features){
+        //loop through each year
+        for(var year = 2013; year <= 2020; year+=1){
+              //get population for current year
+              var value = country.properties[String(year)];
+              value = Number(parseFloat(value.replace(/,/g, '')))
+              //add value to array
+              allValues.push(value);
+        }
+    }
+    //get minimum value of our array
+    var minValue = Math.min(...allValues)
+
+    return minValue;
+}
+
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //constant factor adjusts symbol sizes evenly
+    var minRadius = 5;
+    //Flannery Apperance Compensation formula
+    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+
+    return radius;
+};
+
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng, attributes){
+    //Step 4: Assign the current attribute based on the first index of the attributes array
+    var attribute = attributes[0];
+
+    //create marker options
+    var options = {
+        fillColor: "#E3676E",
+        color: "#ECABAC",
+        weight: 1.5,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(parseFloat(feature.properties[attribute].replace(/,/g, '')))
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content string
+    var popupContent = "<p><b>Country:</b> " + feature.properties.Country + "</p><p><b>Tourists in " + attribute + ":</b> " + feature.properties[attribute] + "</p>";
+
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent, {
+        offset: new L.Point(0,-options.radius) 
+    });
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+function createPropSymbols(data, attributes){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
+        }
+    }).addTo(map);
+};
+
+//Step 10: Resize proportional symbols according to new attribute values
+function updatePropSymbols(attribute){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+            //access feature properties
+            var props = layer.feature.properties;
+
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(Number(parseFloat(props[attribute].replace(/,/g, ''))));
+            layer.setRadius(radius);
+
+            var popupContent = "<p><b>Country:</b> " + props.Country + "</p><p><b>Tourists in " + attribute + ":</b> " + props[attribute] + "</p>";
+
+            //update popup content            
+            popup = layer.getPopup();            
+            popup.setContent(popupContent).update();
+        };
+    });
+};
+
+//Step 1: Create new sequence controls
+function createSequenceControls(attributes){
+    //create range input element (slider)
+    var slider = "<input class='range-slider' type='range'></input>";
+    
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="reverse"></button>');
+    document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="forward"></button>');
+
+    //set slider attributes
+    document.querySelector(".range-slider").max = 7;
+    document.querySelector(".range-slider").min = 0;
+    document.querySelector(".range-slider").value = 0;
+    document.querySelector(".range-slider").step = 1;
+
+    document.querySelector('#reverse').insertAdjacentHTML('beforeend',"<img src='img/reverse.png'>")
+    document.querySelector('#forward').insertAdjacentHTML('beforeend',"<img src='img/forward.png'>")
+
+    //Step 5: click listener for buttons
+    document.querySelectorAll('.step').forEach(function(step){
+        step.addEventListener("click", function(){
+            var index = document.querySelector('.range-slider').value;
+            
+            //Step 6: increment or decrement depending on button clicked
+            if (step.id == 'forward'){
+                index++;
+                //Step 7: if past the last attribute, wrap around to first attribute
+                index = index > 7 ? 0 : index;
+            } else if (step.id == 'reverse'){
+                index--;
+                //Step 7: if past the first attribute, wrap around to last attribute
+                index = index < 0 ? 7 : index;
+            };
+
+            //Step 8: update slider
+            document.querySelector('.range-slider').value = index;
+            
+            //Step 9: pass new attribute to update symbols
+            updatePropSymbols(attributes[index]);
+        })
+    })
+    
+    //Step 5: input listener for slider
+    document.querySelector('.range-slider').addEventListener('input', function(){
+        //Step 6: get the new index value
+        var index = this.value;
+
+        //Step 9: pass new attribute to update symbols
+        updatePropSymbols(attributes[index]);
+    });
+};
+
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        if (attribute.indexOf("20") > -1){
+            attributes.push(attribute);
+        };
+    };
+
+    return attributes;
+};
+
+//Import GeoJSON data
+function getData(map){
     //load the data
     fetch("data/InboundTouristData.geojson")
-        .then(function (response) {
+        .then(function(response){
             return response.json();
         })
-        /*
         .then(function(json){
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(json).addTo(map);
-        });
-        */
-        .then(function (json) {
-            //create marker options
-            var geojsonMarkerOptions = {
-                radius: 10,
-                fillColor: "#E3676E",
-                color: "#ECABAC",
-                weight: 1.5,
-                opacity: 1,
-                fillOpacity: 0.8
-            };
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(json, {
-                onEachFeature: onEachFeature,
-                pointToLayer: function (feature, latlng) {
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-                }
-            }).addTo(map);
-        });
+            //create an attributes array
+            var attributes = processData(json);
+            minValue = calculateMinValue(json);
+            createPropSymbols(json, attributes);
+            createSequenceControls(attributes);
+        })
 };
 
 document.addEventListener('DOMContentLoaded', createMap)
